cd6332377d6085d310203cecca682737bcda0a70
William Hergès <anhgelus@anhgelus.world>
2025-06-19 18:06:09 +0200

    build(gradle): default build file for minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
-group = "world.anhgelus.world.architectsland"
+group = "world.anhgelus.world.architectsland.minecraftscalewayfrontend"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -15,9 +16,31 @@ dependencies {
     implementation("net.minestom:minestom-snapshots:e94aaed297")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Minestom has a minimum Java version of 21
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
     jvmToolchain(21)
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "world.anhgelus.world.architectsland.minecraftscalewayfrontend.Main"
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("") // Prevent the -all suffix on the shadowjar file.
+    }
 }
