fac420c17a98e2d3530971585cf02c704796c6ab
William Hergès <anhgelus@anhgelus.world>
2025-06-19 18:06:20 +0200

    feat(server): simple start and register

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,25 @@
-package world.anhgelus.world.architectsland
+package world.anhgelus.world.architectsland.minecraftscalewayfrontend
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 
 fun main() {
     val name = "Kotlin"
     println("Hello, $name!")
 
+    val server = MinecraftServer.init()
 
+    val instanceManager = MinecraftServer.getInstanceManager()
+
+    val instance = instanceManager.createInstanceContainer()
+    val globalEventHandler = MinecraftServer.getGlobalEventHandler()
+    // spawn player
+    globalEventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
+        val player = event.player
+        event.spawningInstance = instance
+        player.respawnPoint = Pos(0.0, 42.0, 0.0)
+    }
+
+    server.start("0.0.0.0", 25565)
 }
