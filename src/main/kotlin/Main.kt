cbc9de42dbf1ea36867bf93da33dacc5c64fbfa8
William Hergès <anhgelus@anhgelus.world>
2025-06-19 18:18:39 +0200

    feat(event): log connection and chat message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,14 @@ package world.anhgelus.world.architectsland.minecraftscalewayfrontend
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.event.player.PlayerChatEvent
 import net.minestom.server.extras.MojangAuth
 import java.util.logging.Level
 import java.util.logging.Logger
 
-val LOGGER = Logger.getLogger("MinecraftScalewayFrontend")
+val LOGGER: Logger = Logger.getLogger("MinecraftScalewayFrontend")
 
-fun main() {
+fun main(args: Array<String>) {
     LOGGER.level = Level.INFO
     LOGGER.info("Minecraft Scaleway Frontend launched")
     val server = MinecraftServer.init()
@@ -20,13 +21,21 @@ fun main() {
     val instanceManager = MinecraftServer.getInstanceManager()
 
     val instance = instanceManager.createInstanceContainer()
-    val globalEventHandler = MinecraftServer.getGlobalEventHandler()
+    val handler = MinecraftServer.getGlobalEventHandler()
     // spawn player
-    globalEventHandler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
+    handler.addListener(AsyncPlayerConfigurationEvent::class.java) { event ->
+        LOGGER.info("Player ${event.player.name} connected")
         val player = event.player
         event.spawningInstance = instance
         player.respawnPoint = Pos(0.0, 42.0, 0.0)
     }
+
+    handler.addListener(
+        PlayerChatEvent::class.java
+    ) {
+        LOGGER.info(it.formattedMessage.toString())
+    }
+
 
     LOGGER.info("Minecraft Scaleway Frontend started")
     server.start("0.0.0.0", 25565)
