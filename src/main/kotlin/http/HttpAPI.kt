7cd316e50fe94c1b6180849f456d61200ab4ad47
William Hergès <anhgelus@anhgelus.world>
2025-07-15 14:41:40 +0200

    fix(timer): timer cancelled due to GOAWAY http response

--- src/main/kotlin/http/HttpAPI.kt
+++ src/main/kotlin/http/HttpAPI.kt
@@ -1,6 +1,7 @@
 package world.anhgelus.world.architectsland.minecraftscalewayfrontend.http
 
 import world.anhgelus.world.architectsland.minecraftscalewayfrontend.LOGGER
+import java.io.IOException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -9,19 +10,31 @@ import java.net.http.HttpResponse
 abstract class HttpAPI {
     class HttpErrorException(override val message: String?) : RuntimeException(message)
 
-    protected val client: HttpClient = HttpClient.newHttpClient()
+    protected var client: HttpClient = HttpClient.newHttpClient()
 
     protected open fun builder(uri: String): HttpRequest.Builder {
-        return HttpRequest.newBuilder()
-            .uri(URI.create(uri))
+        return HttpRequest.newBuilder().uri(URI.create(uri))
     }
 
-    protected fun send(request: HttpRequest): HttpResponse<String> {
-        val resp = client.send(request, HttpResponse.BodyHandlers.ofString())
+    protected fun send(request: HttpRequest): HttpResponse<String>? {
+        var resp : HttpResponse<String>
+        try {
+            resp = client.send(request, HttpResponse.BodyHandlers.ofString())
+        } catch (e: IOException) {
+            if (e.message == null || !e.message!!.contains("GOAWAY")) throw e
+            resetClient()
+            resp = client.send(request, HttpResponse.BodyHandlers.ofString())
+        }
+        resetClient()
         if (resp.statusCode() >= 400) {
             LOGGER.info(resp.body())
             throw HttpErrorException("Invalid server response status code: ${resp.statusCode()}")
         }
         return resp
+    }
+
+    private fun resetClient() {
+        client.close()
+        client = HttpClient.newHttpClient()
     }
 }
